be289e08457395417dbd8485e6d430910de16d79
smontoro <sarahjmontoro@gmail.com>
2017-11-11 19:23:23 -0800

    updated html

--- js/main.js
+++ js/main.js
@@ -1,108 +1,83 @@
-var objShow= [	
-	{	number: "1.",
-		questionImg:"img/question0.jpg",
+var questions = [	
+	{
+		question:"img/question0.jpg",
 		answer:"Good Times"
 	},
 
-	{	number: "2.",
-		questionImg:"img/question1.jpg",
+	{	
+		question:"img/question1.jpg",
 		answer:"Happy Days"
 	},
 
-	{   number: "3.",
-		questionImg: "img/question2.jpg",
+	{  
+		question: "img/question2.jpg",
 		answer:"Laverne and Shirley"
 	},
 	
 	{	
-		number: "4.",
-		questionImg: "img/question3.jpg",
+		question: "img/question3.jpg",
 		answer:"Sanford and Son"
 	},
 
 	{
-		number: "5.",
-		questionImg: "img/question4.jpg",
+		question: "img/question4.jpg",
 		answer:"Taxi"
 	},
 
 	{
-		number: "6.",
-		questionImg: "img/question5.jpg",
+		question: "img/question5.jpg",
 		answer:"Three's Company"
 	}
 ]
 
-	//creates Q&A section
-	for (var i = 0; i < objShow.length; i++) {
-		//This stores the .main div in the main var
-		var main = document.getElementById("main")
+// loop through each questions
+for(var i = 0; i < questions.length; i++) {
+	// store each question value
+	var question = questions[i].question;
+	// store each element to add question too
+	var el = document.getElementById('question' + [i]);
 
-		var contentDiv = document.createElement("div")
-		contentDiv.className = "mt-5 container"
-		main.appendChild(contentDiv)
+	var img = document.createElement("img")
+	img.setAttribute("src", questions[i].question)
+	img.className = "img-fluid"
 
-		var rowDiv = document.createElement("div")
-		rowDiv.className = "row"
-		contentDiv.appendChild(rowDiv)
+	el.appendChild(img)
+}
 
-		var col5Div = document.createElement("div")
-		col5Div.className = "col-lg-5"
-		var showImg = document.createElement("img")
-		showImg.setAttribute("src", objShow[i].questionImg)
-		showImg.setAttribute("id", "showImg")
-		showImg.className = ("img-fluid")
-		col5Div.appendChild(showImg)
-		rowDiv.appendChild(col5Div)
+// check results when the user submits quiz
+function testResults() {
+	// store variables to update correct and incorrect totals
+	var correct = 0;
+	var incorrect = 0;
 
-		var col4Div = document.createElement("div")
-		col4Div.className = "col-lg-4 mt-5"
-		var answerInput = document.createElement("input")
-		answerInput.className = "text-center"
-		answerInput.setAttribute("placeholder", "Your Answer Here")
-		answerInput.setAttribute("id", "answerInput")
-		col4Div.appendChild(answerInput)
-		rowDiv.appendChild(col4Div)
-
-	
-	}
-
-
-
-	//checks results when button is clicked to submit quiz
-	function resultsFunction() {
-		// store variables to update correct and incorrect totals
-		var correct = 0;
-		var incorrect = 0;
-
-		//loop through objects
-		for(var i = 0; i < objShow.length; i++) {
-			//stores each correct answer
-			var answer = objShow[i].answer 
-			// store each user answer
-			var guess = document.getElementById("answerInput").value
-			// store element to add a class if correct or incorrect
-			var questionSpot = document.getElementById("showImg")
-	}
+// loop through questions array
+	for(var i = 0; i < questions.length; i++) {
+		// store each correct answer
+		var answer = questions[i].answer;
+		// store each user answer
+		var guess = document.getElementById('answer' + [i]).value;
+		// store element to add a class if correct or incorrect
+		var questionSpot = document.getElementById('question' + [i]);
 
 		// check if the user answer matches the correct answer
-			if(answer == guess) {
-				// update class on questionSpot
-				questionSpot.className = 'correct';
-				// add one to correct
-				correct++;
-			} else {
-				// update class on questionSpot
-				questionSpot.className = 'incorrect';
-				// add one to incorrect
-				incorrect++;
-		}
+		if(answer == guess) {
+			// update class on questionSpot
+			questionSpot.className = 'correct';
+			// add one to correct
+			correct++;
+		} else {
+			// update class on questionSpot
+			questionSpot.className = 'incorrect';
+			// add one to incorrect
+			incorrect++;
+		};
+	};
+
 	// update correct and incorrect values
 	document.getElementById('correct').textContent = correct;
 	document.getElementById('incorrect').textContent = incorrect;
-}		
 
-	
+}
 
 
 
