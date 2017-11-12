87a406487ac96a7a603f43ae964da6fa6521a91b
smontoro <sarahjmontoro@gmail.com>
2017-11-11 17:55:42 -0800

    added results section

--- js/main.js
+++ js/main.js
@@ -33,31 +33,79 @@ var objShow= [
 	}
 ]
 
+	//creates Q&A section
 	for (var i = 0; i < objShow.length; i++) {
-		//This stores the .main div in the main vaR
+		//This stores the .main div in the main var
 		var main = document.getElementById("main")
+
 		var contentDiv = document.createElement("div")
 		contentDiv.className = "mt-5 container"
 		main.appendChild(contentDiv)
+
 		var rowDiv = document.createElement("div")
 		rowDiv.className = "row"
 		contentDiv.appendChild(rowDiv)
+
 		var col5Div = document.createElement("div")
 		col5Div.className = "col-lg-5"
 		var showImg = document.createElement("img")
 		showImg.setAttribute("src", objShow[i].questionImg)
+		showImg.setAttribute("id", "showImg")
 		showImg.className = ("img-fluid")
 		col5Div.appendChild(showImg)
 		rowDiv.appendChild(col5Div)
+
 		var col4Div = document.createElement("div")
 		col4Div.className = "col-lg-4 mt-5"
 		var answerInput = document.createElement("input")
 		answerInput.className = "text-center"
 		answerInput.setAttribute("placeholder", "Your Answer Here")
+		answerInput.setAttribute("id", "answerInput")
 		col4Div.appendChild(answerInput)
 		rowDiv.appendChild(col4Div)
 
+	
 	}
+
+
+
+	//checks results when button is clicked to submit quiz
+	function resultsFunction() {
+		// store variables to update correct and incorrect totals
+		var correct = 0;
+		var incorrect = 0;
+
+		//loop through objects
+		for(var i = 0; i < objShow.length; i++) {
+			//stores each correct answer
+			var answer = objShow[i].answer 
+			// store each user answer
+			var guess = document.getElementById("answerInput").value
+			// store element to add a class if correct or incorrect
+			var questionSpot = document.getElementById("showImg")
+	}
+
+		// check if the user answer matches the correct answer
+			if(answer == guess) {
+				// update class on questionSpot
+				questionSpot.className = 'correct';
+				// add one to correct
+				correct++;
+			} else {
+				// update class on questionSpot
+				questionSpot.className = 'incorrect';
+				// add one to incorrect
+				incorrect++;
+		}
+	// update correct and incorrect values
+	document.getElementById('correct').textContent = correct;
+	document.getElementById('incorrect').textContent = incorrect;
+}		
+
+	
+
+
+
 
 
 
