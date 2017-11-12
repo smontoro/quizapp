da62c87b27c63c20216efb0b11dc043e3f6613d3
smontoro <sarahjmontoro@gmail.com>
2017-11-11 17:08:08 -0800

    added elements in js

--- js/main.js
+++ js/main.js
@@ -1,59 +1,61 @@
 var objShow= [	
-	{
-		question: "img/question0.jpg",
+	{	number: "1.",
+		questionImg:"img/question0.jpg",
 		answer:"Good Times"
 	},
 
-	{
-		question: "img/question1.jpg",
+	{	number: "2.",
+		questionImg:"img/question1.jpg",
 		answer:"Happy Days"
 	},
 
-	{  
-		question: "img/question2.jpg",
+	{   number: "3.",
+		questionImg: "img/question2.jpg",
 		answer:"Laverne and Shirley"
 	},
 	
-	{
-		question: "img/question3.jpg",
+	{	
+		number: "4.",
+		questionImg: "img/question3.jpg",
 		answer:"Sanford and Son"
 	},
 
 	{
-		question: "img/question4.jpg",
+		number: "5.",
+		questionImg: "img/question4.jpg",
 		answer:"Taxi"
 	},
 
 	{
-		question: "img/question5.jpg",
+		number: "6.",
+		questionImg: "img/question5.jpg",
 		answer:"Three's Company"
 	}
 ]
 
 	for (var i = 0; i < objShow.length; i++) {
-		//This stores the .main div in the main var
-		var question = objShow[i].question
+		//This stores the .main div in the main vaR
 		var main = document.getElementById("main")
-		var cardDiv = document.createElement("div")
-		cardDiv.className = "card, col-6"
-		main.appendChild(cardDiv)
-		var ol = document.createElement("ol")
-		cardDiv.appendChild(ol)
-		var li = document.createElement("li")
-		ol.appendChild(li)
+		var contentDiv = document.createElement("div")
+		contentDiv.className = "mt-5 container"
+		main.appendChild(contentDiv)
+		var rowDiv = document.createElement("div")
+		rowDiv.className = "row"
+		contentDiv.appendChild(rowDiv)
+		var col5Div = document.createElement("div")
+		col5Div.className = "col-lg-5"
 		var showImg = document.createElement("img")
-		showImg.setAttribute("src", question)
-		showImg.className = "showImg, card-img-top"
-		li.appendChild(showImg)
-		/*var cardBodyDiv = document.createElement("div")
-		cardBodyDiv.className = "card-body"
-		cardDiv.appendChild(cardBodyDiv)
-		var showImg = document.createElement("img")
-		showImg.setAttribute("src", question)
-		showImg.className = "showImg, card-img-top"
-		cardBodyDiv.appendChild(showImg)
-		var inputAnswer = document.createElement("input")
-		cardBodyDiv.appendChild(inputAnswer)*/
+		showImg.setAttribute("src", objShow[i].questionImg)
+		showImg.className = ("img-fluid")
+		col5Div.appendChild(showImg)
+		rowDiv.appendChild(col5Div)
+		var col4Div = document.createElement("div")
+		col4Div.className = "col-lg-4 mt-5"
+		var answerInput = document.createElement("input")
+		answerInput.className = "text-center"
+		answerInput.setAttribute("placeholder", "Your Answer Here")
+		col4Div.appendChild(answerInput)
+		rowDiv.appendChild(col4Div)
 
 	}
 
